5adaa95378cde2c19a966f1f4d64eed815447b2e
Amen Atikpo <alaskale2024@gmail.com>
2022-06-13 07:43:56 +0000

    Looping along lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const array = ["Guadalupe", "Ollie", "Aki"];
+const strg = "surprise";
+
+function writeCards(array, strg="surpise"){
+    let newArray =[];
+    for(let i=0; i<array.length;i++) {
+        newArray.push("Thank you, "+array[i]+", for the wonderful "+strg+" gift!");  
+    }
+    return newArray;
+}
+function countDown(number){
+    for(let i=number;i>=0;i--){
+        console.log(i);
+    }
+}
